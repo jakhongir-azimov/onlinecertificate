35c5b423d18fffa4bd1b5629cb2c00569445d00f
Jakhongir <azimovjahongir19@gmail.com>
2023-04-29 16:24:59 +0500

    added model

---
+++ Certificate/src/main/java/com/company/certificate/model/Direction.java
@@ -0,0 +1,17 @@
+package com.company.certificate.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = ("directions"))
+public class Direction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
---
+++ Certificate/src/main/java/com/company/certificate/model/User.java
@@ -0,0 +1,18 @@
+package com.company.certificate.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = ("users"))
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+}
